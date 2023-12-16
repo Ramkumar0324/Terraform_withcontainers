3014e5f9971400001acfdfcbf1238f611271ef0c
Ramkumar0324 <107704493+Ramkumar0324@users.noreply.github.com>
2023-12-16 13:01:06 +0530

    Update 05_iam.tf

--- terraform/05_iam.tf
+++ terraform/05_iam.tf
@@ -1,22 +1,22 @@
 resource "aws_iam_role" "ecs-host-role" {
   name               = "ecs_host_role_prod"
-  assume_role_policy = file("C:/Users/0324r/Terraform_wc/Terraform_withcontainers/terraform/policies/ecs-role.json")
+  assume_role_policy = file("/d/terraform/Terraform_withcontainers/terraform/policies/ecs-role.json")
 }
 
 resource "aws_iam_role_policy" "ecs-instance-role-policy" {
   name   = "ecs_instance_role_policy"
-  policy = file("C:/Users/0324r/Terraform_wc/Terraform_withcontainers/terraform/policies/policies/ecs-instance-role-policy.json")
+  policy = file("/d/terraform/Terraform_withcontainers/terraform/policiess/policies/ecs-instance-role-policy.json")
   role   = aws_iam_role.ecs-host-role.id
 }
 
 resource "aws_iam_role" "ecs-service-role" {
   name               = "ecs_service_role_prod"
-  assume_role_policy = file("C:/Users/0324r/Terraform_wc/Terraform_withcontainers/terraform/policies/ecs-role.json")
+  assume_role_policy = file("/d/terraform/Terraform_withcontainers/terraform/policies/ecs-role.json")
 }
 
 resource "aws_iam_role_policy" "ecs-service-role-policy" {
   name   = "ecs_service_role_policy"
-  policy = file("C:/Users/0324r/Terraform_wc/Terraform_withcontainers/terraform/policies/ecs-service-role-policy.json")
+  policy = file("/d/terraform/Terraform_withcontainers/terraform/policies/ecs-service-role-policy.json")
   role   = aws_iam_role.ecs-service-role.id
 }
 
